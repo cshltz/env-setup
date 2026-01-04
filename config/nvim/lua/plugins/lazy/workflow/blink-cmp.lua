6ccf6a3fe201454c43f1d0fff60a2ef951687476
Caleb <cshultz117@gmail.com>
2026-01-03 16:05:54 -0800

    add inline completions and integrat NES with tab in blink.

--- config/nvim/lua/plugins/lazy/workflow/blink-cmp.lua
+++ config/nvim/lua/plugins/lazy/workflow/blink-cmp.lua
@@ -41,6 +41,16 @@ return {
         -- See :help ins-completion
 
         preset = 'default',
+        ['<Tab>'] = {
+          'snippet_forward',
+          function() -- sidekick next edit suggestion
+            return require('sidekick').nes_jump_or_apply()
+          end,
+          function() -- if you are using Neovim's native inline completions
+            return vim.lsp.inline_completion.get()
+          end,
+          'fallback',
+        },
         ['<C-j>'] = { 'select_next', 'fallback' },
         ['<C-k>'] = { 'select_prev', 'fallback' },
       },
